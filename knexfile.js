2afb95fa49cf3b47ad3f8e9988d93d580df01f24
Brian Hague <alphaseinor@gmail.com>
2020-11-04 12:23:15 -0600

    configured knexfile for pg on dev, test, and prod

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {directory: './data/seeds'},
+  },
+
+
+  test: {
+    client: 'pg',
+    connection: process.env.TEST_DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {directory: './data/seeds'},
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {directory: './data/seeds'},
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+};
